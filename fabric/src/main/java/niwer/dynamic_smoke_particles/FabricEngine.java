c53e81a121528538f859d15f28d16d7a1a99a942
Niwer <erwin.redote@gmail.com>
2026-06-12 15:32:45 +0200

    Javadoc, Config, and mixin - Moved mixin to common - Load the config on each loader - Moved the mixin into the common using the new common config - Removed server from gradle configs since this mod is client side only - Updated FAQ to remove the loader question/answer

--- fabric/src/main/java/niwer/dynamic_smoke_particles/FabricEngine.java
+++ fabric/src/main/java/niwer/dynamic_smoke_particles/FabricEngine.java
@@ -4,22 +4,14 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screens.Screen;
 import niwer.dynamic_smoke_particles.compatibility.ClothConfigCompat;
-import niwer.dynamic_smoke_particles.config.Config;
 import niwer.dynamic_smoke_particles.platform.Services;
 
-public class FabricEngine implements ClientModInitializer {
+public class FabricEngine extends Engine implements ClientModInitializer {
 	private static final String[] CLOTH_CONFIG_ID = new String[] { "cloth-config", "cloth_config", "cloth-config2" };
-
-	private static Config configuration;
 
 	@Override
 	public void onInitializeClient() {
-		Config.setConfigPath(FabricLoader.getInstance().getConfigDir());
-		configuration = Config.loadConfig();
-	}
-
-	public static Config config() {
-		return configuration;
+		this.init(FabricLoader.getInstance().getConfigDir());
 	}
 
 	/**
@@ -29,7 +21,7 @@ public class FabricEngine implements ClientModInitializer {
 	 * @return The config screen or null if no compatible config mod is loaded.
 	 */
 	public static Screen getConfigScreen(Screen parent) {
-		if(Services.PLATFORM.isModLoaded(CLOTH_CONFIG_ID)) return ClothConfigCompat.getConfigScreen(parent, configuration);
+		if(Services.PLATFORM.isModLoaded(CLOTH_CONFIG_ID)) return ClothConfigCompat.getConfigScreen(parent, config());
 		return null;
 	}
 }
--- neoforge/src/main/java/niwer/dynamic_smoke_particles/NeoForgeEngine.java
+++ neoforge/src/main/java/niwer/dynamic_smoke_particles/NeoForgeEngine.java
@@ -3,10 +3,12 @@ package niwer.dynamic_smoke_particles;
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLLoader;
 
 @Mod(Constants.MOD_ID)
-public class NeoForgeEngine {
+public class NeoForgeEngine extends Engine {
 
     public NeoForgeEngine(IEventBus eventBus) {
+        this.init(FMLLoader.getCurrent().getGameDir());
     }
 }
